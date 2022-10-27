6d055d0167e278be2bccbaf922d82b86fde80ce3
gmmkmtgk <keshavmaheshwari800@gmail.com>
2022-10-28 02:05:05 +0530

    Added Arch

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("here")
+require('dotenv').config()
+
+const express = require('express')
+const productRoutes = require('./routes/product')
+
+// express app
+const app = express()
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.get('/', (req, res) => {
+  res.json({mssg: 'Welcome to the app'})
+})
+app.use('/api/product', productRoutes)
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT)
+})
